24b9a1b450a14d615d41a813907636c22535d2fc
Samil Abud <samilabud@gmail.com>
2025-06-30 18:54:48 -0400

    fix: adds connection

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
 const apiUrl =
-  import.meta.env.VITE_API_URL || "http://backend-2634705220:8000/api";
+  import.meta.env.VITE_API_URL || "/choreo-apis/djangopractices/backend/v1";
 const api = axios.create({
   baseURL: apiUrl,
   headers: {
